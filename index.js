e6fe97a1e991724e05b4ffaaf293a1fb15445d81
Bilal <blalsnmez.8@gmail.com>
2022-02-13 11:33:06 +0300

    First Commit, Routes and mongo db init

---
+++ index.js
@@ -0,0 +1,17 @@
+const { dbUserName, dbPassword } = require('./config/db');
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const express = require('express');
+const app = express();
+
+mongoose.connect(`mongodb+srv://${dbUserName}:${dbPassword}@cluster0.vfgfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+.then(() => console.log('mongo connected'))
+.catch(err => console.error('mongo not connect', err));
+
+app.use(express.json());
+app.use('api/users', users);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Listening Port: ' + port));
