fd31c7342c37d8feb91d2caea3914371ec51884b
Bilal <blalsnmez.8@gmail.com>
2022-02-15 22:34:20 +0300

    Edit Product Route

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const { checkAuth } = require('../models/auth');
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
-    const products = await Product.findOne({_id: req.params.id});
+    const products = await Product.findOne({contentLink: req.params.id});
     if (products) {
         res.send(products);
     }
