81064997c13e8046bad562b29174226c9e62ba08
Bilal <blalsnmez.8@gmail.com>
2022-02-17 17:40:07 +0300

    register edited

--- routes/auth.js
+++ routes/auth.js
@@ -61,20 +61,14 @@ router.get('/check', async (req, res) => {
 router.post('/register', async (req, res) => {
     const { error } = validateUser(req.body);
     if (error) {
-        return res.status(400).send(error.details[0].message);
+        return res.status(400).send({status: false, message: error.details[0].message});
     }
 
     let user = await User.findOne({ email: req.body.email });
     if (user) {
         return res.status(400).send({ status: false, message: 'User Already Exists!' });
     }
-    user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        userName: req.body.userName,
-        email: req.body.email,
-        password: req.body.password,
-    });
+    user = new User(req.body);
     user = new User(loDash.pick(req.body, ['name', 'email', 'password']));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
